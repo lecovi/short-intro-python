e37b11664c2b33b1472206b2be9340c4a6f6f0ac
Leandro E. Colombo Viña <lecovi@zenbook>
2020-04-14 00:12:38 -0300

    Adds operadores relacionales

---
+++ operadores_relacionales.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+## Los operadores relacioales devuelven valores lógicos.
+
+a = 10
+b = 30
+print("Comparemos 2 valores enteros:")
+print("-"*50)
+print("La variable a vale {}, la variable b {}. Entonces ¿a es igual a b?: {}".format(a, b, a == b))
+print("La variable a vale {}, la variable b {}. Entonces ¿a es distinta a b?: {}".format(a, b, a != b))
+print("La variable a vale {}, la variable b {}. Entonces ¿a es mayor a b?: {}".format(a, b, a > b))
+print("La variable a vale {}, la variable b {}. Entonces ¿a es menor a b?: {}".format(a, b, a < b))
+print("La variable a vale {}, la variable b {}. Entonces ¿a es mayor o igual a b?: {}".format(a, b, a >= b))
+print("La variable a vale {}, la variable b {}. Entonces ¿a es menor o igual a b?: {}".format(a, b, a <= b))
+print("="*60)
+
+a = 12
+b = 25.5
+
+print("Comparemos 1 entero con 1 real:")
+print("-"*50)
+print("La variable a vale {}, la variable b {}. Entonces ¿a es igual a b?: {}".format(a, b, a == b))
+print("La variable a vale {}, la variable b {}. Entonces ¿a es distinta a b?: {}".format(a, b, a != b))
+print("La variable a vale {}, la variable b {}. Entonces ¿a es mayor a b?: {}".format(a, b, a > b))
+print("La variable a vale {}, la variable b {}. Entonces ¿a es menor a b?: {}".format(a, b, a < b))
+print("La variable a vale {}, la variable b {}. Entonces ¿a es mayor o igual a b?: {}".format(a, b, a >= b))
+print("La variable a vale {}, la variable b {}. Entonces ¿a es menor o igual a b?: {}".format(a, b, a <= b))
+print("="*60)
